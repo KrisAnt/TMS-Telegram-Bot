036d030a9f8747405ffd3fb27b4c333090d199f1
KrisAnt <62154371+KrisAnt@users.noreply.github.com>
2020-05-22 14:01:05 +0300

    fix: fixed service problems, recreated web-app

---
+++ Antonova.RESTAURANTTELEGRAMBOT/Antonova.KennyWest/Extensions/TBExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace Antonova.KennyWest.Extensions
+{
+    public static class TBExtension
+    {
+        /// <summary>
+        /// Add TelegramBot client.
+        /// </summary>
+        public static IServiceCollection AddTelegramBotClient(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            var client = new TelegramBotClient(configuration["Token"]);
+            var webHook = $"{configuration["Url"]}/api/message/update";
+            client.SetWebhookAsync(webHook).Wait();
+
+            serviceCollection.AddTransient<ITelegramBotClient>(x => client);
+
+            return serviceCollection;
+
+        }
+    }
+}
--- Antonova.RESTAURANTTELEGRAMBOT/Antonova.KennyWest/Startup.cs
+++ Antonova.RESTAURANTTELEGRAMBOT/Antonova.KennyWest/Startup.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Antonova.Bot.Extensions;
 using Antonova.Core.Interfaces;
 using Antonova.Core.Services;
+using Antonova.KennyWest.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -15,7 +15,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 
-namespace Antonova.Bot
+namespace Antonova.KennyWest
 {
     public class Startup
     {
@@ -29,29 +29,23 @@ namespace Antonova.Bot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHealthChecks();
             services.AddScoped<ICommandService, CommandService>();
             services.AddTelegramBotClient(Configuration);
             services.AddControllers()
-                 .AddNewtonsoftJson(options =>
-                 {
-                     options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
-                     options.SerializerSettings.ContractResolver = new DefaultContractResolver();
-                 });
-        } 
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                });
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-         
             app.UseRouting();
-
-           
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHealthChecks("/health");
             });
         }
     }
